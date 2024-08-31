078623125a6ca9dd0deb08366cf1f8ef127fbc30
OrgestPasha <orgestpasha2005@gmail.com>
2024-09-01 01:38:04 +0200

    actual start to the snake game

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ let move = false;
 let blobX = 0;
 let blobY = 0;
 
+let speed = 250; //this would be the interval we request the animation feature in turn allowing us to speed up in later levels
+
 let direction = "";
 
+//here we have the declaration of direction and assigning to it we have to do this before we request the animation
+//since we have to know what to animate
 document.addEventListener("keydown", (event) => {
   switch (event.code) {
     case "Space":
@@ -30,29 +34,40 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
+//actual animation for movement
 function moveBlob() {
   if (move) {
     switch (direction) {
       case "w":
         console.log("w");
-        blobY -= 5;
+        blobY -= 25;
         break;
 
       case "a":
-        blobX -= 5;
+        blobX -= 25;
         break;
 
       case "s":
-        blobY += 5;
+        blobY += 25;
         break;
 
       case "d":
-        blobX += 5;
+        blobX += 25;
         break;
     }
 
+    //constricting the position of the snake within bounds
+    blobX = Math.max(0, Math.min(blobX, 375));
+    blobY = Math.max(0, Math.min(blobY, 575));
+
     blob.style.transform = `translate(${blobX}px, ${blobY}px)`;
   }
-  requestAnimationFrame(moveBlob);
+
+  //to get the retro feel and for better implementation we request frames at a certain lower speed
+  setTimeout(() => {
+    requestAnimationFrame(moveBlob);
+  }, speed);
 }
+
+//initial call so our program animates when we initialize
 moveBlob();
